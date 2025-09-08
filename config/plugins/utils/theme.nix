c1ebd4b9c215ece6e1e139117718d21ba8bd2f53
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2025-09-08 13:15:41 +0800

    feat: theme ingration

--- config/plugins/utils/theme.nix
+++ config/plugins/utils/theme.nix
@@ -3,6 +3,18 @@
     enable = true;
     settings = {
       style = "night";
+      plugins = {
+        blink = true;
+        bufferline = true;
+        dap = true;
+        fzf = true;
+        mini = true;
+        rainbow = true;
+        snacks = true;
+        treesitter = true;
+        treesitter-context = true;
+        which-key = true;
+      };
     };
   };
 }
