d98b766f003829c77eeb3284c83a2a2b8df54b1d
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2026-04-07 23:39:46 +0800

    fix(fzf): keymap config

--- plugins/utils/fzf-lua.nix
+++ plugins/utils/fzf-lua.nix
@@ -1,5 +1,21 @@
 {
   plugins.fzf-lua = {
     enable = true;
+    keymaps = {
+      "<leader><leader>" = "files";
+      "<leader>/" = "live_grep";
+      "<leader>fg" = "live_grep";
+      "<leader>ff" = "files";
+      "<leader>fb" = "buffers";
+      "<leader>fc" = {
+        action = "git_files";
+        options = {
+          silent = true;
+          desc = "Fzf-Lua Git Files";
+        };
+      };
+      "<leader>fh" = "help_tags";
+      "<leader>fr" = "oldfiles";
+    };
   };
 }
