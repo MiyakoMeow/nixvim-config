bf9699f1f30fcad2b8e887e48f4579cda4d0df89
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2026-04-07 20:22:06 +0800

    fix(settings): add leader config

--- config/settings.nix
+++ config/settings.nix
@@ -1,11 +1,13 @@
 {
-  performance.byteCompileLua = {
-    enable = true;
-    configs = true;
-    initLua = true;
-    luaLib = true;
-    nvimRuntime = true;
-    plugins = true;
+  config = {
+    performance.byteCompileLua = {
+      enable = true;
+      configs = true;
+      initLua = true;
+      luaLib = true;
+      nvimRuntime = true;
+      plugins = true;
+    };
   };
 
   options = {
