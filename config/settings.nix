12d44d9e8ec9e4222dccbdbdb65c9f7a068da31d
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2026-04-07 23:24:39 +0800

    fix(settings): sync (#14)

--- config/settings.nix
+++ config/settings.nix
@@ -1,5 +1,6 @@
 {
   config = {
+    # Performance
     performance.byteCompileLua = {
       enable = true;
       configs = true;
@@ -11,14 +12,29 @@
   };
 
   config.opts = {
+    # Indentation
     expandtab = true;
     shiftwidth = 4;
     tabstop = 4;
     softtabstop = 0;
     smarttab = true;
+
+    # Encoding
+    fileencoding = "utf-8";
+
+    # Scrolling
+    scrolloff = 6;
+    sidescrolloff = 6;
+
+    # Numbers
+    number = true;
+    relativenumber = false;
   };
 
   config.globals = {
+    # Leader
     mapleader = " ";
+    # Encoding
+    encoding = "UTF-8";
   };
 }
