d1150579107c7deb6918ec8816ea977515e1efa2
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2026-04-07 18:18:00 +0800

    add: neo-tree with log_to_file=false

---
+++ plugins/editor/neo-tree.nix
@@ -0,0 +1,21 @@
+{
+  plugins.neo-tree = {
+    enable = true;
+    settings = {
+      log_to_file = false;
+      logging.level = "error";
+    };
+  };
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>e";
+      action.__raw = "function() require('neo-tree.command').execute({ toggle = true }) end";
+      options = {
+        desc = "Neo-tree";
+        silent = true;
+      };
+    }
+  ];
+}
