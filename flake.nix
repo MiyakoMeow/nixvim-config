370fc1e1111fca3c607a8af06f1e88c7962f57c4
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2026-04-07 15:17:47 +0800

    fix: list systems

--- flake.nix
+++ flake.nix
@@ -21,12 +21,7 @@
       ...
     }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "aarch64-linux"
-        "x86_64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
+      systems = nixpkgs.lib.systems.flakeExposed;
 
       perSystem =
         {
