fd7d79f9ad2b66e7e5d44e2d560d45089ae4b8d6
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2026-05-06 01:58:28 +0800

    refactor: flatten plugins attrset, reduce indentation
    用 plugins.xxx 替代 plugins = { xxx = ... }，去一层嵌套。

--- plugins/feature/treesitter.nix
+++ plugins/feature/treesitter.nix
@@ -1,27 +1,25 @@
 { pkgs, ... }:
 {
-  plugins = {
-    treesitter = {
-      enable = true;
+  plugins.treesitter = {
+    enable = true;
 
-      settings = {
-        indent = {
-          enable = true;
-        };
-        highlight = {
-          enable = true;
-        };
-        incremental_selection = {
-          enable = true;
-        };
+    settings = {
+      indent = {
+        enable = true;
       };
-
-      nixvimInjections = true;
-      grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
+      highlight = {
+        enable = true;
+      };
+      incremental_selection = {
+        enable = true;
+      };
     };
 
-    rainbow-delimiters = {
-      enable = true;
-    };
+    nixvimInjections = true;
+    grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
+  };
+
+  plugins.rainbow-delimiters = {
+    enable = true;
   };
 }
